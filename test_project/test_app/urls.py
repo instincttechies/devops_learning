4e9de6295b084cf3236cf3826a7039021f6873d0
instincttechies <91719972+instincttechies@users.noreply.github.com>
2024-01-05 21:03:52 +0530

    Update urls.py

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup/', views.signup,name='signup'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('accounts/login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('summary/', views.summary, name='summary'),
     path('signout/', views.signout, name='signout'),
+    path('api/register/', views.UserRegistrationAPIView.as_view(), name='register'),
+    # path('api/login/', views.UserLoginAPIView.as_view(), name='login'),
+    # path('api/logout/', views.UserLogoutAPIView.as_view(), name='logout'),
 ]
